3396e17d9bd31abefa74455547b95ba832da77c9
coldclimate <oli@canddi.com>
2014-07-21 12:42:10 +0100

    first cut, scripts only

---
+++ grab.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+while read line
+do
+	CLEAN=`echo ${line} | sed 's/[^a-zA-Z0-9]/_/g'`
+	wget "http://www.${line}/robots.txt" -O data/${CLEAN}.txt
+done < sites.lst
+git commit -am "DEAL WITH IT" && git push -f origin master
