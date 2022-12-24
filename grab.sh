902dcdc42c17ce04d8c0679a7f1572d7b2a38b19
coldclimate <>
2022-12-24 16:26:56 +0000

    shout out where we are for debugging

--- grab.sh
+++ grab.sh
@@ -4,4 +4,5 @@ while read line
 do
 	CLEAN=`echo ${line} | sed 's/[^a-zA-Z0-9]/_/g'`
 	curl -Lsk --connect-timeout 2 "http://www.${line}/robots.txt" -o data/${CLEAN}.txt
+	echo ${CLEAN}
 done < sites.lst
